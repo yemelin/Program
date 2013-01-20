3cbbba6ebf3b6037d4cd944524b223f764bf533f
yemelin <yemelin@gmail.com>
2013-01-20 19:57:31 +0400

    Update src/constructionsite/GuiAttributes.java
    Упрощен код обработки нажатия кнопки, исправлена проблема с точностью
    float, возникавшая при систематическом вычитании разового копания из 
    глубины ямы.

--- src/constructionsite/GuiAttributes.java
+++ src/constructionsite/GuiAttributes.java
@@ -10,7 +10,7 @@ package constructionsite;
 public class GuiAttributes {
     // Константы для инициализации
 
-    final int startMoney = 10000; // Сумма тендера (сколько заплатят после выполнения работы)
+    final int startMoney = 3000; // Сумма тендера (сколько заплатят после выполнения работы)
     final int startCaveJob = 10;  // Объем работы (требуемая глубина ямы)
     final float mechDigDepth = 1.0f; // Глубина ямы в метрах, выкопанной за 1раз экскаватором
     final int mechDigCost = 800;     // Стоимость в USD 1раза копания экскаватором
@@ -29,29 +29,23 @@ public class GuiAttributes {
     Result result = Result.NEW;
 
     void diggerButtonAction() {
-        money = money - mechDigCost;
-        caveDepth = caveDepth + mechDigDepth;
-        diggerEnable = (money >= mechDigCost);
-        //ShovelEnableJ=(money>=nonmechDigCost);
-        if (startCaveJob - caveDepth <= 0) {
-            caveDepth = startCaveJob; // потому, что глубина должна быть не больше заказанной, а меньше 1раза оплачивается, как 1 раз
-            diggerEnable = false;
-            shovelEnableJ = false;
-            result = Result.WIN;
-        } else {
-            if (money < nonmechDigCost) {
-                diggerEnable = false;
-                shovelEnableJ = false;
-                result = Result.LOSE;
-            }
-        }
+        genericButtonAction(mechDigCost,mechDigDepth);
     }
 
     void shovelButtonAction() {
-        money = money - nonmechDigCost;
-        caveDepth = caveDepth + nonmechDigDepth;
-        //DiggerEnableJ=(money>=mechDigCost);
+        genericButtonAction(nonmechDigCost,nonmechDigDepth);
+    }
+    
+    void genericButtonAction(int DigCost, float DigDepth){
+        money = money - DigCost;
+//float precision handling        
+        caveDepth = (float)(Math.round((caveDepth + DigDepth)*10))/10;
+        diggerEnable=(money>=mechDigCost);
         shovelEnableJ = (money >= nonmechDigCost);
+        checkResult();
+    }
+    
+    void checkResult(){
         if (startCaveJob - caveDepth <= 0) {
             caveDepth = startCaveJob; // потому, что глубина должна быть не больше заказанной, а меньше 1раза оплачивается, как 1 раз
             diggerEnable = false;
